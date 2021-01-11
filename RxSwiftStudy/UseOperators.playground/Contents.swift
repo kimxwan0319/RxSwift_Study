273f8b9fea9bc1a3cd87248064cba935864bb335
kimxwan0319 <kimxwan0319@naver.com>
2021-01-11 17:17:44 +0900

    Update; UserOperators > justExample1

--- RxSwiftStudy/UseOperators.playground/Contents.swift
+++ RxSwiftStudy/UseOperators.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 import RxSwift
 
+func justExample1(){
+    Observable.just("value")
+        .subscribe(onNext: { str in
+            print(str)
+        })
+}
+
+
+// MARK: - Execution
+
+justExample1()
+
