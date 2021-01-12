b944cd85f66dbce75cc0bb400b10652e12a8b354
kimxwan0319 <kimxwan0319@naver.com>
2021-01-13 01:26:09 +0900

    UPDATE; UseOperators( processing dispose

--- RxSwiftStudy/UseOperators.playground/Contents.swift
+++ RxSwiftStudy/UseOperators.playground/Contents.swift
@@ -1,11 +1,14 @@
 import UIKit
 import RxSwift
 
+var disposeBag = DisposeBag()
+
 func justExample1(){
     Observable.just("value")
         .subscribe(onNext: { str in
             print(str)
         })
+        .disposed(by: disposeBag)
 }
 
 
