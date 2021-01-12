e45204f597c47bb30560a69e8cadd977ae26e443
kimxwan0319 <kimxwan0319@naver.com>
2021-01-13 01:42:39 +0900

    UPDATE; UseOperators( fromExample1

--- RxSwiftStudy/UseOperators.playground/Contents.swift
+++ RxSwiftStudy/UseOperators.playground/Contents.swift
@@ -5,14 +5,28 @@ var disposeBag = DisposeBag()
 
 func justExample1(){
     Observable.just("value")
-        .subscribe(onNext: { str in
-            print(str)
+        .subscribe(onNext: { res in
+            print(res)
+        }, onCompleted: {
+            print("com")
+        })
+        .disposed(by: disposeBag)
+}
+
+func fromExample1(){
+    Observable.from(["val1","val2"])
+        .subscribe(onNext: { res in
+            print(res)
+        }, onCompleted: {
+            print("com")
         })
         .disposed(by: disposeBag)
 }
 
 
+
+
 // MARK: - Execution
 
-justExample1()
+fromExample1()
 
